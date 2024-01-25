8b6b327d515328cc8c2679c3035e840e4926fbdf
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2024-01-25 23:59:20 +0300

    Adding EntityFramewrok. Database connection. Entity relationships

---
+++ JwtApp/Persistance/Configurations/ProductConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using JwtApp.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JwtApp.Persistance.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+
+        builder.HasOne(x => x.Categoty).WithMany(x => x.Products)
+            .HasForeignKey(x => x.CategoryId);
+    }
+}
---
+++ JwtApp/Persistance/Context/JwtContext.cs
@@ -0,0 +1,26 @@
+﻿using JwtApp.Core.Domain;
+using JwtApp.Persistance.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace JwtApp.Persistance.Context;
+
+public class JwtContext : DbContext
+{
+    public JwtContext(DbContextOptions<JwtContext> options) : base(options) {}
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Categoty> Categories { get; set; }
+    public DbSet<AppUser> AppUsers { get; set; }
+    public DbSet<AppRole> AppRole { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        //modelBuilder.ApplyConfiguration(new AppUserConfiguration());
+
+        //base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
